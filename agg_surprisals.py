c2055e26f7cfb83106e1894c8ff0344ded19d0bf
Jenn <jhu890@gmail.com>
2019-08-08 14:09:24 -0400

    initial cli unk support

--- agg_surprisals.py
+++ agg_surprisals.py
@@ -1,26 +1,17 @@
 import utils
 import argparse
-import subprocess
 import pandas as pd
 from pathlib import Path
 from region import Sentence, Region
 
 class TokenMismatch(Exception):
-    pass
+    def __init__(self, token1, token2, t_idx):
+        msg = '''
+tokens \"%s\" and \"%s\" do not match (line %d in surprisal file)
+        ''' % (token1, token2, t_idx)
+        Exception.__init__(self, msg)
 
-def tokenize_file(sentence_path, model):
-    if model is None:
-        with open(sentence_path, 'r') as f:
-            sentences = f.readlines()
-    else:
-        # need to call external script to avoid hanging PIPE
-        cmd = './tokenize_file %s %s' % (model, sentence_path)
-        sentences = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
-        sentences = sentences.stdout.decode('utf-8').split('\n')
-    tokens = [s.strip().split(' ') for s in sentences]
-    return tokens
-
-def get_agg_surprisals(surprisals, sentence_tokens, in_data, model):
+def agg_surprisals(surprisals, sentence_tokens, unks, in_data, model):
     # check that specified metrics are implemented in utils.METRICS
     metrics = in_data['meta']['metric']
     if metrics == 'all':
@@ -28,23 +19,26 @@ def get_agg_surprisals(surprisals, sentence_tokens, in_data, model):
     else:
         utils.validate_metrics(metrics)
 
-    # initialize counters for token and sentence
+    # store tokens and surprisal values from surprisal file
+    TOKENS = surprisals['token'].values
+    SURPRISALS = surprisals['surprisal'].values
+
+    # initialize counters for token and sentence from in_data
     t_idx, s_idx = 0, 0
+
     # iterate through surprisal file, matching tokens with regions
     for i_idx, item in enumerate(in_data['items']):
         for c_idx, cond in enumerate(item['conditions']):
             sentence = Sentence(**cond, tokens=sentence_tokens[s_idx])
             for r_idx, region in enumerate(sentence.regions):
                 for token in region.tokens:
-                    # find matching surprisal value for token
-                    if token == surprisals['token'].values[t_idx]:
-                        s = surprisals['surprisal'].values[t_idx]
-                        region.token_surprisals.append(s)
+                    # find matching surprisal value for token,
+                    # or simply store surprisal if token is UNK
+                    if (token == TOKENS[t_idx] or unks[t_idx] == 1):
+                        region.token_surprisals.append(SURPRISALS[t_idx])
                         t_idx += 1
                     else:
-                        raise TokenMismatch('''
-tokens \"%s\" and \"%s\" do not match (line %d in surprisal file)
-                        ''' % (token, surprisals['token'].values[t_idx], t_idx+2))
+                        raise TokenMismatch(token, TOKENS[t_idx], t_idx + 2)
                 # get dictionary of aggregate surprisal values for each metric
                 vals = {m : region.agg_surprisal(m) for m in metrics}
                 # add to original dict
@@ -57,8 +51,9 @@ tokens \"%s\" and \"%s\" do not match (line %d in surprisal file)
 def main(args):
     in_data = utils.load_json(args.i)
     surprisals = pd.read_csv(args.surprisal, delim_whitespace=True)
-    sentence_tokens = tokenize_file(args.sentences, args.model)
-    out_data = get_agg_surprisals(surprisals, sentence_tokens, in_data, args.model)
+    sentence_tokens = utils.tokenize_file(args.sentences, args.model)
+    unks = utils.unkify_file(args.sentences, args.model)
+    out_data = agg_surprisals(surprisals, sentence_tokens, unks, in_data, args.model)
     utils.write_json(out_data, args.o)
 
 if __name__ == '__main__':
@@ -70,7 +65,8 @@ if __name__ == '__main__':
     parser.add_argument('--model', '-model', '--m', '-m', type=str,
                         choices=utils.MODELS, default=None, 
                         help='model for cpllab Docker container. '
-                             'if None, then split tokens by spaces')
+                             'if None, then split tokens by spaces and treat'
+                             'all tokens as in-vocabulary (no UNKs)')
     parser.add_argument('--i', '-i', type=Path,
                         help='path to JSON file with input data')
     parser.add_argument('--o', '-o', type=Path,
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 from inspect import getargspec
+import subprocess
 
 METRICS = {
     'sum': sum,
@@ -61,8 +62,52 @@ def write_lines(lines, path):
             f.write(str(l) + '\n')
 
 def validate_metrics(metrics):
+    """
+    Checks if specified metrics are valid. Returns None if check passes,
+    else raises ValueError.
+    """
     # if only one metric specified, convert to list
     metrics = [metrics] if len(metrics) == 1 else metrics
     if any(m not in METRICS for m in metrics):
         bad_metrics = [m for m in metrics if m not in METRICS]
         raise ValueError('Unknown metrics: {}'.format(bad_metrics))
+
+def run(cmd_str):
+    """
+    Runs specified command using subprocess and returns list of lines
+    from stdout.
+    """
+    res = subprocess.run(cmd_str.split(), stdout=subprocess.PIPE)
+    return res.stdout.decode('utf-8').split('\n')
+
+def tokenize_file(sentence_path, model):
+    """
+    Tokenizes file at sentence_path according to model.
+    If model is None, then split tokens based on whitespace.
+    """
+    if model is None:
+        with open(sentence_path, 'r') as f:
+            sentences = f.readlines()
+    else:
+        # need to call external script to avoid hanging PIPE
+        cmd = './tokenize_file %s %s' % (model, sentence_path)
+        sentences = run(cmd)
+    tokens = [s.strip().split(' ') for s in sentences]
+    return tokens
+    
+def unkify_file(sentence_path, model):
+    """
+    Unkifies file at sentence_path according to model.
+    If model is None, then return no unks (all 0s).
+    Note that the returned list is flattened over sentences.
+    """
+    if model is None:
+        tokens = tokenize_file(sentence_path, model)
+        unks = [0 for sentence in tokens for t in sentence]
+    else:
+        # need to call external script to avoid hanging PIPE
+        cmd = './unkify_file %s %s' % (model, sentence_path)
+        unk_mask = run(cmd)
+        unk_mask = [u for u in unk_mask if u != '']
+        unks = [int(s) for u in unk_mask for s in u.split(' ')]
+    return unks
