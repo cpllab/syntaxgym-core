b6b9cc5bd41f7cec9281c897a999032d5b397234
Jon Gauthier <jon@gauthiers.net>
2019-09-12 14:04:27 -0400

    cli: support working with arbitrary images

--- agg_surprisals.py
+++ agg_surprisals.py
@@ -11,7 +11,7 @@ tokens \"%s\" and \"%s\" do not match (line %d in surprisal file)
         ''' % (token1, token2, t_idx)
         Exception.__init__(self, msg)
 
-def agg_surprisals(surprisals, sentence_tokens, unks, in_data, model):
+def agg_surprisals(surprisals, sentence_tokens, unks, in_data, model_name):
     # check that specified metrics are implemented in utils.METRICS
     metrics = in_data['meta']['metric']
     if metrics == 'all':
@@ -45,14 +45,14 @@ def agg_surprisals(surprisals, sentence_tokens, unks, in_data, model):
                 in_data['items'][i_idx]['conditions'][c_idx]['regions'][r_idx]['metric_value'] = vals
             # update sentence counter
             s_idx += 1
-    in_data['meta']['model'] = model
+    in_data['meta']['model'] = model_name
     return in_data
 
 def main(args):
     in_data = utils.load_json(args.i)
     surprisals = pd.read_csv(args.surprisal, delim_whitespace=True)
-    sentence_tokens = utils.tokenize_file(args.sentences, args.model)
-    unks = utils.unkify_file(args.sentences, args.model)
+    sentence_tokens = utils.tokenize_file(args.sentences, args.image)
+    unks = utils.unkify_file(args.sentences, args.image)
     out_data = agg_surprisals(surprisals, sentence_tokens, unks, in_data, args.model)
     utils.write_json(out_data, args.o)
 
@@ -62,11 +62,9 @@ if __name__ == '__main__':
                         help='path to file containing token-based surprisals')
     parser.add_argument('--sentences', '-sentences', type=Path,
                         help='path to file containing sentences')
-    parser.add_argument('--model', '-model', '--m', '-m', type=str,
-                        choices=utils.MODELS, default=None, 
-                        help='model for cpllab Docker container. '
-                             'if None, then split tokens by spaces and treat'
-                             'all tokens as in-vocabulary (no UNKs)')
+    parser.add_argument('--model', '-m', type=str,
+                        help='model name')
+    parser.add_argument("--image", type=str, help="Docker image name")
     parser.add_argument('--i', '-i', type=Path,
                         help='path to JSON file with input data')
     parser.add_argument('--o', '-o', type=Path,
