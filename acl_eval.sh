9acb28071c73d0fc07544e47dfc367ceab4fb59c
Jenn <jhu890@gmail.com>
2019-12-03 18:05:18 -0500

    fix mvrr preds

--- acl_eval.sh
+++ acl_eval.sh
@@ -6,7 +6,8 @@ SEED=$3
 SURPRISAL_ROOT="/Users/jennhu/Desktop/lm-zoo/syntaxgym/analysis/surprisal/"
 SUITE_ROOT="/Users/jennhu/Desktop/lm-zoo/syntaxgym/test-suites"
 
-mkdir -p "${SURPRISAL_ROOT}/${MODEL_NAME}/${CORPUS}/result"
+RESULT_DIR="${SURPRISAL_ROOT}/${MODEL_NAME}/${CORPUS}/json"
+mkdir -p ${RESULT_DIR}
 MODEL="${MODEL_NAME}_${CORPUS}_${SEED}"
 
 suites=($(ls ${SUITE_ROOT}/json))
@@ -16,10 +17,10 @@ do
     SUITE_NAME=$(echo "$suite" | cut -f 1 -d '.')
     echo $SUITE_NAME
     python agg_surprisals.py \
-        -surprisal ${SURPRISAL_ROOT}/${MODEL_NAME}/${CORPUS}/${SUITE_NAME}_${MODEL}.csv \
+        -surprisal ${SURPRISAL_ROOT}/${MODEL_NAME}/${CORPUS}/csv/${SUITE_NAME}_${MODEL}.csv \
         -sentences ${SUITE_ROOT}/token/${SUITE_NAME}.txt \
         -m ${MODEL} \
         -image cpllab/language-models:tiny-lstm \
         -i ${SUITE_ROOT}/json/${SUITE_NAME}.json \
-        -o ${SURPRISAL_ROOT}/${MODEL_NAME}/${CORPUS}/result/${SUITE_NAME}_${MODEL}.json
+        -o ${RESULT_DIR}/${SUITE_NAME}_${MODEL}.json
 done
