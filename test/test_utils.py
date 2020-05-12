a53efd4c6222ff2a9fe14610d0590f8545fdfd25
Jon Gauthier <jon@gauthiers.net>
2020-05-12 09:44:40 -0400

    hopefully get CLI tests working portably

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,7 +3,7 @@ import tempfile
 
 import pytest
 
-from utils import *
+import utils
 
 
 @pytest.fixture(scope="function")
@@ -15,13 +15,15 @@ def sentence_file():
         yield f
 
 
-@pytest.fixture()
-def image():
-    return "cpllab/language-models:grnn"
+@pytest.fixture(scope="module", params=["cpllab/language-models:grnn"])
+def image(request):
+    image_ref, tag = request.param.rsplit(":", 1)
+    utils._pull_container(image_ref, tag)
+    return ":".join((image_ref, tag))
 
 
 def test_get_spec(image):
-    get_spec(image)
+    utils.get_spec(image)
 
 
 def test_tokenize(sentence_file, image):
@@ -29,4 +31,4 @@ def test_tokenize(sentence_file, image):
     expected = expected.split("\n")
     expected = [sentence.split(" ") for sentence in expected]
 
-    assert tokenize_file(sentence_file.name, image) == expected
+    assert utils.tokenize_file(sentence_file.name, image) == expected
