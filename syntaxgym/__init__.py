fd8412ee3c2d40bf284a83489dc5a9d8634412b0
Jon Gauthier <jon@gauthiers.net>
2020-05-14 14:38:45 -0400

    factor out compute_surprisals method

--- syntaxgym/__init__.py
+++ syntaxgym/__init__.py
@@ -1,2 +1,32 @@
+import json
+
+from lm_zoo import spec, tokenize, unkify, get_surprisals
+
+from syntaxgym import utils
 from syntaxgym.agg_surprisals import aggregate_surprisals
 from syntaxgym.get_sentences import get_sentences
+
+__version__ = "0.1"
+
+
+def compute_surprisals(model_name, suite_file):
+    if not hasattr(suite_file, "read"):
+        suite_file = open(suite_file, "r")
+
+    image_spec = spec(model_name)
+    suite = json.load(suite_file)
+
+    # Convert to sentences
+    suite_sentences = get_sentences(suite)
+
+    # First compute surprisals
+    surprisals_df = get_surprisals(model_name, suite_sentences)
+
+    # Track tokens+unks
+    tokens = tokenize(model_name, suite_sentences)
+    unks = unkify(model_name, suite_sentences)
+
+    # Now aggregate over regions and get result df
+    result = aggregate_surprisals(surprisals_df, tokens, unks, suite, image_spec)
+
+    return result
