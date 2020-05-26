13f6f70f33515a6055a70830d15efb6711783c41
Jon Gauthier <jon@gauthiers.net>
2020-05-26 14:24:08 -0400

    update compute_surprisals to be compatible with lm-zoo 1.1

--- syntaxgym/__init__.py
+++ syntaxgym/__init__.py
@@ -1,6 +1,6 @@
 import json
 
-from lm_zoo import spec, tokenize, unkify, get_surprisals
+from lm_zoo import get_registry, spec, tokenize, unkify, get_surprisals
 import pandas as pd
 
 from syntaxgym import utils
@@ -24,13 +24,16 @@ def _load_suite(suite_ref):
         suite = suite_ref
     return Suite.from_dict(suite)
 
+def _load_model(model_ref):
+    return get_registry()[model_ref]
 
-def compute_surprisals(model_name, suite):
+
+def compute_surprisals(model_ref, suite):
     """
     Compute per-region surprisals for a language model on the given suite.
 
     Args:
-        model_name: Reference to an LM Zoo model (either a model in the
+        model_ref: Reference to an LM Zoo model (either a model in the
             registry or a Docker image reference)
         suite_file: A path or open file stream to a suite JSON file, or an
             already loaded suite dict
@@ -40,17 +43,18 @@ def compute_surprisals(model_name, suite):
         ``suite_file``, now including per-region surprisal data
     """
     suite = _load_suite(suite)
-    image_spec = spec(model_name)
+    model = _load_model(model_ref)
+    image_spec = spec(model)
 
     # Convert to sentences
     suite_sentences = get_sentences(suite)
 
     # First compute surprisals
-    surprisals_df = get_surprisals(model_name, suite_sentences)
+    surprisals_df = get_surprisals(model, suite_sentences)
 
     # Track tokens+unks
-    tokens = tokenize(model_name, suite_sentences)
-    unks = unkify(model_name, suite_sentences)
+    tokens = tokenize(model, suite_sentences)
+    unks = unkify(model, suite_sentences)
 
     # Now aggregate over regions and get result df
     result = aggregate_surprisals(surprisals_df, tokens, unks, suite, image_spec)
